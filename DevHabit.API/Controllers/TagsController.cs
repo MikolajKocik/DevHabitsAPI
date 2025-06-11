74226fb00c5d6b59449d4bd2457ef0fce28b09b4
Mikołaj Kocik <mikolajkocik@onet.pl>
2025-06-11 15:16:13 +0200

    tags

---
+++ DevHabit.API/Controllers/TagsController.cs
@@ -0,0 +1,100 @@
+﻿using System.Diagnostics;
+using DevHabit.API.Database;
+using DevHabit.API.DTOs.Tags;
+using DevHabit.API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevHabit.API.Controllers;
+
+[ApiController]
+[Route("tags")]
+public sealed class TagsController(ApplicationDbContext dbContext) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<TagsCollectionDto>> GetTags()
+    {
+        List<TagDto> tags = await dbContext
+            .Tags
+            .Select(TagsQueries.ProjectToDto())
+            .ToListAsync();
+
+        var habitsCollectionDto = new TagsCollectionDto
+        {
+            Data = tags
+        };
+
+        return Ok(habitsCollectionDto);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<TagDto>> GetTag(string id)
+    {
+        TagDto? tag = await dbContext
+            .Tags
+            .Where(h => h.Id == id)
+            .Select(TagsQueries.ProjectToDto())
+            .FirstOrDefaultAsync();
+
+        if(tag is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(tag);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> CreateTag(CreateTagDto createTagDto)
+    {
+        Tag tag = createTagDto.ToEntity();
+
+        if(await dbContext.Tags.AnyAsync(t => t.Name == tag.Name))
+        {
+            return Conflict($"The tag '{tag.Name}' already exists");
+        }
+
+        dbContext.Tags.Add(tag);
+
+        await dbContext.SaveChangesAsync();
+
+        TagDto tagDto = tag.ToDto();
+
+        return CreatedAtAction(nameof(GetTag), new { id = tagDto.Id }, tagDto);
+    }
+
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> UpdateTag(string id, UpdateTagDto updateTagDto)
+    {
+        Tag? tag = await dbContext.Tags.FirstOrDefaultAsync(h => h.Id == id);
+
+        if(tag is null)
+        {
+            return NotFound();
+        }
+
+        tag.UpdateFromDto(updateTagDto);
+
+        await dbContext.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteTag(string id)
+    {
+        Tag? tag = await dbContext.Tags.FirstOrDefaultAsync(h => h.Id == id);
+
+        if (tag is null)
+        {
+            return NotFound();
+        }
+
+        dbContext.Tags.Remove(tag);
+
+        await dbContext.SaveChangesAsync();
+
+        return NoContent();
+    }
+}
---
+++ DevHabit.API/DTOs/Tags/TagsQueries.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+using DevHabit.API.Entities;
+
+namespace DevHabit.API.DTOs.Tags;
+
+public static class TagsQueries
+{
+    public static Expression<Func<Tag, TagDto>> ProjectToDto()
+    {
+        return t => new TagDto
+        { 
+            Id = t.Id,
+            Name = t.Name,
+            Description = t.Description,
+            CreatedAtUtc = t.CreatedAtUtc,
+            UpdatedAtUtc = t.UpdatedAtUtc
+        };
+
+    }
+}
